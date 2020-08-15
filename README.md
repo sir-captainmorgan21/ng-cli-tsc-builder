1e28dcbf8deb16196711021377759a514c0f91cb
Pierre <pierre.machaux@gmail.com>
2020-08-15 16:27:16 -0400

    improve

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
 # Ng Cli Tsc Builder
 
-A simple Angular cli builder using only tsc and file replacement
+A simple Angular cli builder using only tsc and file replacement.
+
+## Use case
+
+When you have a project you want to build with the Angular CLI without webpack.
+We use only the tsc compiler.
+
+## Accepted options:
+
+    - tsConfig: Path to tsconfig file used for the compilation
+    - outputPath: Folder to output build
+    - rootDir: See --rootDir in https://www.typescriptlang.org/docs/handbook/compiler-options.html
+    - sourceMap: Include sourceMap
+    - fileReplacements: Replace file by another one (same as usual)
+
+## Process
+
+    - Clean previous dist folder
+    - Build using TS compiler and options provided
+    - Replace files if fileReplacements is provided
+    - Clean all unused environment files
+
+## Example of usage (within Nx monorepo)
+
+```
+"my-app": {
+      "root": "apps/my-app",
+      "sourceRoot": "apps/my-app/src",
+      "projectType": "application",
+      "prefix": "my-app",
+      "schematics": {},
+      "architect": {
+        "build": {
+          "builder": "@pmachaux/ng-cli-tsc-builder:build",
+          "options": {
+            "outputPath": "dist/apps/mlk/my-app",
+            "tsConfig": "apps/my-app/tsconfig.app.json",
+            "rootDir": "apps/my-app/src",
+            "sourceMap": true
+          },
+          "configurations": {
+            "production": {
+              "fileReplacements": [
+                {
+                  "replace": "apps/my-app/src/environments/environment.ts",
+                  "with": "apps/my-app/src/environments/environment.prod.ts",
+                }
+              ],
+              "sourceMap": false
+            }
+          }
+        },
+        "lint": {
+          "builder": "@angular-devkit/build-angular:tslint",
+          "options": {
+            "tsConfig": [
+              "apps/my-app/tsconfig.app.json",
+              "apps/my-app/tsconfig.spec.json"
+            ],
+            "exclude": ["**/node_modules/**", "!apps/my-app/**"]
+          }
+        }
+      }
+    }
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { BuilderOutput, createBuilder } from "@angular-devkit/architect";
 import * as childProcess from "child_process";
+import * as fs from "fs";
 import { JsonObject } from "@angular-devkit/core";
 import { join } from "path";
 
@@ -8,9 +9,67 @@ interface Options extends JsonObject {
   args: string[];
 }
 
-const fileReplacementsBuilder = (fileReplacements: any): Promise<number> => {
+const cleanUnusedEnvfiles = (distPath: string, context: any) => {
   return new Promise<number>((resolve, reject) => {
-    resolve(0);
+    if (distPath) {
+      const envRegex = `${distPath}/environments/environment.*.js`;
+      const child = childProcess.spawn(`npx rimraf ${envRegex}`, [], { shell: true });
+      child.stderr.on("error", (data) => {
+        context.logger.error(data.toString());
+        reject(1);
+      });
+      context.reportStatus(`Done.`);
+      child.on("close", (code) => {
+        resolve(code);
+      });
+    } else {
+      resolve(0);
+    }
+  });
+};
+
+const fileReplacementsBuilder = (
+  fileReplacements: Array<{ replace: string; with: string }>,
+  distPath: string,
+  rootDir: string,
+  context: any
+): Promise<number> => {
+  return new Promise<number>((resolve, reject) => {
+    const filesWrites = fileReplacements.map((r) => {
+      return new Promise((fResolve, fReject) => {
+        const withPath = distPath + r.with.replace(rootDir, "").replace(".ts", ".js");
+        context.logger.info(JSON.stringify(withPath));
+        fs.readFile(withPath, (errRead, data) => {
+          if (errRead) {
+            context.logger.error(JSON.stringify(errRead));
+            fReject();
+          }
+          const replacePath = distPath + r.replace.replace(rootDir, "").replace(".ts", ".js");
+          context.logger.info(JSON.stringify(replacePath));
+          fs.writeFile(replacePath, data, (errWrite) => {
+            if (errWrite) {
+              context.logger.error(JSON.stringify(errWrite));
+              fReject();
+            }
+            fs.unlink(withPath, (errClean) => {
+              if (errClean) {
+                context.logger.error(JSON.stringify(errClean));
+                fReject();
+              }
+              fResolve();
+            });
+          });
+        });
+      });
+    });
+    Promise.all(filesWrites).then(
+      () => {
+        resolve(0);
+      },
+      () => {
+        resolve(1);
+      }
+    );
   });
 };
 
@@ -20,7 +79,7 @@ const cleanDistFolder = (outDirFolder: string, context: any): Promise<number> =>
       const child = childProcess.spawn(`npx rimraf ${outDirFolder}`, [], { shell: true });
       child.stderr.on("error", (data) => {
         context.logger.error(data.toString());
-        reject();
+        reject(1);
       });
       context.reportStatus(`Done.`);
       child.on("close", (code) => {
@@ -33,37 +92,45 @@ const cleanDistFolder = (outDirFolder: string, context: any): Promise<number> =>
 };
 
 export default createBuilder<Options>((options, context) => {
-  return new Promise(async (resolve, reject) => {
+  return new Promise<BuilderOutput>(async (resolve, reject) => {
     context.reportStatus(`Executing tsc build --${options.tsConfig}...`);
-    const distFolder = join(process.cwd(), (options.outputPath as string) || "");
-    const outDir = options.outputPath ? `--outDir ${distFolder}` : "";
+
+    if (!options.outputPath) {
+      context.logger.error("You need to provide outputPath in angular.json");
+    }
+    const distFolder = join(process.cwd(), options.outputPath as string);
+    const outDir = `--outDir ${distFolder}`;
     const rootDir = options.rootDir ? `--rootDir ${options.rootDir}` : "";
+    const sourceMap = `--sourceMap ${!!options.sourceMap}`;
     const fileReplacements = options.fileReplacements;
 
     const cleanDistFolderCode = await cleanDistFolder(options.outputPath ? distFolder : "", context);
     if (cleanDistFolderCode) {
-      reject();
+      reject(1);
     }
 
-    const child = childProcess.spawn(`npx tsc --project ${options.tsConfig} ${outDir} ${rootDir}`, [], { shell: true });
+    const child = childProcess.spawn(`npx tsc --project ${options.tsConfig} ${outDir} ${rootDir} ${sourceMap}`, [], { shell: true });
     child.stdout.on("data", (data) => {
       context.logger.info(data.toString());
     });
     child.stderr.on("error", (data) => {
       context.logger.error(data.toString());
-      reject();
+      reject(1);
     });
     context.reportStatus(`Done.`);
-    child.on("close", (code) => {
-      if (!!fileReplacements) {
+    child.on("close", async (code) => {
+      if (!fileReplacements) {
         resolve({ success: code === 0 });
       } else {
         if (code !== 0) {
           resolve({ success: false });
         }
-        fileReplacementsBuilder(fileReplacements).then((fCode) => {
-          resolve({ success: fCode === 0 });
-        });
+        const fReplacementCode = await fileReplacementsBuilder(fileReplacements as any, distFolder, (options.rootDir as string) || "", context);
+        if (fReplacementCode) {
+          resolve({ success: false });
+        }
+        const cleanEnvFilesCode = await cleanUnusedEnvfiles(distFolder, context);
+        resolve({ success: cleanEnvFilesCode === 0 });
       }
     });
   });
